63e2c203d611845e151419a9400404972d7087d2
Jagadeeswar J <115331104+JagadeeswarJ@users.noreply.github.com>
2025-04-22 10:29:19 +0000

    Add friend function to class A for accessing private members

--- 07_friend.cpp
+++ 07_friend.cpp
@@ -5,6 +5,8 @@ class A
 {
     string secret = "password";
     friend class B; // friend class, b will have access to private members of A
+    // friend function, show will have access to private members of A
+    friend void show(A a);
 };
 class B
 {
@@ -14,11 +16,16 @@ public:
         cout << a.secret << endl; // error
     }
 };
+void show(A a)
+{
+    cout << a.secret << endl; // error
+}
 int main()
 {
     cout << "Friend" << endl;
     A a1;
     B b1;
     b1.show(a1);
+    show(a1);
     return 0;
 }
